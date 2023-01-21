bf577b826f3a0baa8eb9e239ece396c687879291
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2023-01-21 14:13:58 +0530

    init

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+// const ejs = require('ejs')
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000 , (req, res)=>{
+    console.log(`Site is live on 3000`)
+})
